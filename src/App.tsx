c9038cc9648ed4242b265d1bd8621f6b0ec404e8
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-07-18 13:46:20 +0530

    Add a basic header

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
+import { AppHeader } from "./components/AppHeader";
 import { HomeScreen } from "./components/HomeScreen";
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-      {/* {loading ? (
-          <div>loading...</div>
-        ) : (
-          <div>loaded {JSON.stringify(response)}</div>
-        )} */}
-      {/* </header> */}
+      <AppHeader />
       <HomeScreen />
     </div>
   );
---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const AppHeader = () => (
+  <header style={{ width: 100, backgroundColor: "white", height: 48 }}>
+    <img src="./8-flix.png" style={{ height: "100%" }} />
+  </header>
+);
